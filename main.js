6992adacc2629fc6a91c459819083d6881c5d527
Jan Jakub Zábranský <zabransky@SPSEJECNANET.CZ>
2023-03-30 09:09:17 +0200

    final commit

--- main.js
+++ main.js
@@ -2,13 +2,21 @@ const genBtn = document.getElementById('genBtn');
 const likeBtn = document.getElementById('likeBtn');
 const dslikeBtn = document.getElementById('dislikeBtn');
 let jokeTxt = document.getElementById('jokeText');
+const likedListBtn = document.getElementById('likedListBtn');
+const dislikedListBtn = document.getElementById('dislikedListBtn');
+const likedJokes = [];
+const dislikedJokes = [];
 
 likeBtn.style.visibility = 'hidden';
 dslikeBtn.style.visibility = 'hidden';
+likedListBtn.style.visibility = 'hidden';
+dislikedListBtn.style.visibility = 'hidden';
 
 genBtn.addEventListener('click', Connect);
 likeBtn.addEventListener('click', Like);
 dslikeBtn.addEventListener('click', Dislike);
+likedListBtn.addEventListener('click', LikedList);
+dislikedListBtn.addEventListener('click', DislikedList);
 
 function Connect() {
     $.ajax({
@@ -25,6 +33,8 @@ function Connect() {
             }
             likeBtn.style.visibility = 'visible';
             dslikeBtn.style.visibility = 'visible';
+            likedListBtn.style.visibility = 'visible';
+            dislikedListBtn.style.visibility = 'visible';
         },
         error: function(error) {
             console.log("Server is down");
@@ -33,10 +43,17 @@ function Connect() {
 }
 
 function Like() {
-
+    likedJokes.push(response.joke);
 }
 
 function Dislike() {
+    dislikedJokes.push(response.joke);
+}
+
+function DislikedList() {
 
 }
 
+function LikedList() {
+
+}
